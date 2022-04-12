9750a9767ec8e76e00d885dc1288fd823c34ff81
sevajal <sevajal@yahoo.com>
2022-04-12 15:54:28 -0500

    we finished shell 0.1?

--- shell.c
+++ shell.c
@@ -1,16 +1,5 @@
 #include "main.h"
 
-void free_memory(char **tokens, char *token, char *string)
-{
-	int i;
-	for (i = 0; tokens != NULL; i++)
-		free(tokens[i]);
-	free(tokens);
-	free(token);
-	free(string);
-
-}
-
 /**
  * main - interactive command line interpreter
  * @argc - number of arguments
@@ -19,25 +8,29 @@ void free_memory(char **tokens, char *token, char *string)
 
 int main(void)
 {       /*  int argc, char **argv[], extern char **environ; */
-	char *string, **tokens, *token;
+	char *string;
 	size_t n = 0;
 	ssize_t gl;
 	pid_t fork_id, w_pid; /*w_pid, pid, ppid*/
 	int i;
 	/*char *exe_envp[] = { "PATH=$PATH", NULL };*/
+	char **tokens;
+	char *token;
 
 	do{
-		string = NULL, token = NULL, tokens = NULL;
+		string = NULL;
 		n = 0, i = 0;
 
 		if (write (STDOUT_FILENO, "($) ", 4) == -1) /* prints the prompt*/
+		{
 			dprintf(STDERR_FILENO, "Can't write the stdout");
-
+		}
+		
 		/* ssize_t getline(char **restrict lineptr, size_t *restrict n, FILE *restrict stream); */
 		gl = getline(&string, &n, stdin);/* read the line */
 		if (gl == -1)
 		{
-			free_memory(tokens, token, string);
+			free(string);
 			if(feof(stdin))
 				return (EXIT_SUCCESS);
 			else
@@ -46,18 +39,11 @@ int main(void)
 		
 		if ((tokens = malloc(10 * sizeof(char *))) == NULL)
 		{
-			i++, token = strtok(NULL, " ");
-			printf("%d, ", i);
-			fflush(stdout);
-		}
-		if ((tokens = malloc((i - 1) * sizeof(char *))) == NULL)
-		{
-			free_memory(NULL, token, string);
+			free(tokens);
 			return(EXIT_FAILURE); /* EXIT_SUCCESS or EXIT_FAILURE */
 		}
 
 		token = strtok(string, " \t\n");
-		i = 0;
 		while (token != NULL)
 		{
 			tokens[i] = token;
@@ -97,10 +83,7 @@ int main(void)
 			free(string);
 			w_pid = wait(NULL);
 			if (w_pid == -1)
-			{
-				free_memory(tokens, token, string);
 				exit(EXIT_FAILURE);
-			}
 		}
 	} while (1);
 	free(tokens);
